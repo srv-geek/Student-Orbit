1f751284f97e55951356011a9001de928c4aa91d
Shubham Raj Verma <124357448+srv-geek@users.noreply.github.com>
2025-03-09 19:30:37 +0530

    Update Subject.java

--- src/main/java/com/tka/studentorbit/api/entity/Subject.java
+++ src/main/java/com/tka/studentorbit/api/entity/Subject.java
@@ -11,27 +11,47 @@ public class Subject {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String assignedFaculty;
 
     public Subject() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	public Long getId() {
+     public Subject(Long id, String name, String assignedFaculty) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.assignedFaculty = assignedFaculty;
+	}
+
+      public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+       public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getName() {
+       public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
-    
+
+	public String getAssignedFaculty() {
+		return assignedFaculty;
+	}
+
+	public void setAssignedFaculty(String assignedFaculty) {
+		this.assignedFaculty = assignedFaculty;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject [id=" + id + ", name=" + name + ", assignedFaculty=" + assignedFaculty + "]";
+	}
     
 }
 
