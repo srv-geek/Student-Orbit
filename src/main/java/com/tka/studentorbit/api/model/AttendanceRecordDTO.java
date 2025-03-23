25738800571ed05a7e2644b3b565028fe1909b8d
Shubham Raj Verma <124357448+srv-geek@users.noreply.github.com>
2025-03-23 16:38:32 +0530

    Update AttendanceRecordDTO.java

--- src/main/java/com/tka/studentorbit/api/model/AttendanceRecordDTO.java
+++ src/main/java/com/tka/studentorbit/api/model/AttendanceRecordDTO.java
@@ -6,37 +6,37 @@ import java.util.stream.Collectors;
 import com.tka.studentorbit.api.entity.AttendanceRecord;
 
 public class AttendanceRecordDTO {
-    private String id;
-    private String username;
-    private String firstNme; // Assuming you want the faculty's name as well
-    private Long subjectId;
-    private String subjectName; // Assuming you want the subject's name as well
-    private String date;
-    private String time;
-    private int numberOfStudents;
-    private List<StudentDTO> students;
+  private String id;
+	private String username;
+	private String firstNme; // Assuming you want the faculty's name as well
+	private Long subjectId;
+	private String subjectName; // Assuming you want the subject's name as well
+	private String date;
+	private String time;
+	private int numberOfStudents;
+	private List<StudentDTO> students;
 
-    // Constructor
-    public AttendanceRecordDTO(AttendanceRecord record) {
-        this.id = record.getId();
-        this.username = record.getUser().getUsername();
-        this.firstNme = record.getUser().getFirstName(); 
-        this.subjectId = record.getSubject().getId();
-        this.subjectName = record.getSubject().getName();
-        this.numberOfStudents = record.getNumberOfStudents();
-        this.students = record.getStudents().stream().map(StudentDTO::new).collect(Collectors.toList());
-    }
+	// Constructor
+	public AttendanceRecordDTO(AttendanceRecord record) {
+		this.id = record.getId();
+		this.username = record.getUser().getUsername();
+		this.firstNme = record.getUser().getFirstName();
+		this.subjectId = record.getSubject().getId();
+		this.subjectName = record.getSubject().getName();
+		this.numberOfStudents = record.getNumberOfStudents();
+		this.students = record.getStudents().stream().map(StudentDTO::new).collect(Collectors.toList());
+	}
 
-    // Getters and setters
-    public String getId() {
-        return id;
-    }
+	// Getters and setters
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getUsername() {
+	public String getUsername() {
 		return username;
 	}
 
@@ -53,24 +53,22 @@ public class AttendanceRecordDTO {
 	}
 
 	public Long getSubjectId() {
-        return subjectId;
-    }
+		return subjectId;
+	}
 
-    public void setSubjectId(Long subjectId) {
-        this.subjectId = subjectId;
-    }
+	public void setSubjectId(Long subjectId) {
+		this.subjectId = subjectId;
+	}
 
-    public String getSubjectName() {
-        return subjectName;
-    }
+	public String getSubjectName() {
+		return subjectName;
+	}
 
-    public void setSubjectName(String subjectName) {
-        this.subjectName = subjectName;
-    }
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
 
-   
-
-    public String getDate() {
+	public String getDate() {
 		return date;
 	}
 
@@ -87,18 +85,18 @@ public class AttendanceRecordDTO {
 	}
 
 	public int getNumberOfStudents() {
-        return numberOfStudents;
-    }
+		return numberOfStudents;
+	}
 
-    public void setNumberOfStudents(int numberOfStudents) {
-        this.numberOfStudents = numberOfStudents;
-    }
+	public void setNumberOfStudents(int numberOfStudents) {
+		this.numberOfStudents = numberOfStudents;
+	}
 
-    public List<StudentDTO> getStudents() {
-        return students;
-    }
+	public List<StudentDTO> getStudents() {
+		return students;
+	}
 
-    public void setStudents(List<StudentDTO> students) {
-        this.students = students;
-    }
+	public void setStudents(List<StudentDTO> students) {
+		this.students = students;
+	}
 }
